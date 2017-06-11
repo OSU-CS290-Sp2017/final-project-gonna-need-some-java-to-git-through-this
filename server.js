87ff67ae93ce6aa27ec885a8eeecc287dc74b537
Lucas Campos-Davis <camposdl@oregonstate.edu>
2017-06-11 16:45:03 -0700

    added and started on server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var path = require('path');
+var fs = require('fs');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = process.env.PORT || 3000;
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
