a3c89ae00260be3ca43cc8b9a871d88707110239
Lucas Campos-Davis <camposdl@oregonstate.edu>
2017-06-11 17:46:31 -0700

    hopefully finished server

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ app.getapp.get('/News', function (req, res, next) {
 	res.render('news', templateArgs);
 });
 
+app.getapp.get('/Job-Openings', function (req, res, next) {
+	templateArgs{
+		jobs: jobData
+	};
+	res.render('job_openings', templateArgs);
+});
+
+app.getapp.get('/About', function (req, res, next) {
+	templateArgs{
+		people: aboutData
+	};
+	res.render('about', templateArgs);
+});
+
+app.get('*', function (req, res) {
+  res.status(404).render('404Page');
+});
 
 app.listen(port, function(){
 	console.log("Server listening on port:", port);
